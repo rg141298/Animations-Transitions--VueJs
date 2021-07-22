900348e110a1a1cbc95a71a8754e1ecfaae9e0ad
Raman Goyal <ramangoyal@Ramans-MacBook-Air.local>
2021-07-22 23:26:31 +0530

    animations with using javascript

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
       @before-leave='beforeLeave'
       @leave='leave'
       @after-leave='afterLeave'
+      @enter-cancelled='enterCancelled'
+      @leave-cancelled='leaveCancelled'
     >
       <p v-if='paraIsVisible'>This is only sometimes visible...</p>
     </transition>
@@ -43,18 +45,30 @@ export default {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
-      usersAreVisible: false
+      usersAreVisible: false,
+      enterInterval:null,
+      leaveInterval:null,
     };
   },
   methods: {
     beforeEnter(el){
       console.log('beforeEnter');
       console.log(el);
+      el.style.opacity=0;
     },
 
-    enter(el){
+    enter(el,done){
       console.log('enter');
       console.log(el);
+      let round=1;
+      this.enterInterval= setInterval(()=>{
+        el.style.opacity=round*0.01;
+        round++
+        if(round>100){
+          clearInterval(this.enterInterval);
+          done();
+        }
+      },20);
     },
     afterEnter(el){
       console.log('afterEnter');
@@ -63,13 +77,34 @@ export default {
     beforeLeave(el){
       console.log('beforeLeave');
       console.log(el);
+      el.style.opacity=1;
     },
-    leave(el){
+    leave(el,done){
       console.log('leave');
       console.log(el);
+      let round=1;
+      this.leaveInterval= setInterval(()=>{
+        el.style.opacity=1-round*0.01;
+        round++
+        if(round>100){
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      },20);
     },
     afterLeave(el){
       console.log('afterLeave');
+      console.log(el);
+
+    },
+    enterCancelled(el){
+      clearInterval(this.enterInterval)
+      console.log('enterCancelled');
+      console.log(el);
+    },
+    leaveCancelled(el){
+      clearInterval(this.leaveInterval);
+      console.log('leaveCancelled');
       console.log(el);
     },
     showDialog() {
@@ -149,35 +184,6 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.para-enter-from {
-  /*opacity: 0;*/
-  /*transform: translateY(-30px);*/
-}
-
-.para-enter-active {
-  /*transition: all 0.3s ease-out;*/
-  animation: slide-scale 0.3s ease-out;
-}
-
-.para-enter-to {
-  /*opacity: 1;*/
-  /*transform: translateY(0);*/
-}
-
-.para-leave-from {
-  /*opacity: 0;*/
-  /*transform: translateY(0);*/
-}
-
-.para-leave-active {
-  /*transition: all 0.3s ease-in;*/
-  animation: slide-scal 0.3s ease-out;
-}
-
-.para-leave-to {
-  /*opacity: 0;*/
-  /*transform: translateY(-30);*/
-}
 
 .fade-button-enter-from,
 .fade-button-leave-to
