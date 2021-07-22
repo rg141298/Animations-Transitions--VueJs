73f49822805589de6cef993d3ee64052f51484b6
Raman Goyal <ramangoyal@Ramans-MacBook-Air.local>
2021-07-22 22:34:42 +0530

    Using Custom CSS Class names by adding name prop to our transition component

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <button @click='animateBlock'>Animate</button>
   </div>
   <div class='container'>
-    <transition>
+    <transition name='para'>
       <p v-if='paraIsVisible'>This is only sometimes visible...</p>
     </transition>
     <button @click='toggleParagraph'>Toggle Paragraph</button>
@@ -91,31 +91,31 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from{
+.para-enter-from{
   /*opacity: 0;*/
   /*transform: translateY(-30px);*/
 }
 
-.v-enter-active{
+.para-enter-active{
   /*transition: all 0.3s ease-out;*/
   animation: slide-scale 0.3s ease-out;
 }
-.v-enter-to{
+.para-enter-to{
   /*opacity: 1;*/
   /*transform: translateY(0);*/
 }
 
-.v-leave-from{
+.para-leave-from{
   /*opacity: 0;*/
   /*transform: translateY(0);*/
 }
 
-.v-leave-active{
+.para-leave-active{
   /*transition: all 0.3s ease-in;*/
   animation: slide-scal 0.3s ease-out;
 }
 
-.v-leave-to{
+.para-leave-to{
   /*opacity: 0;*/
   /*transform: translateY(-30);*/
 }
