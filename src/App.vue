934d282bdd3a89cadfb960cee3ea7611e1e9de43
Raman Goyal <ramangoyal@Ramans-MacBook-Air.local>
2021-07-22 23:08:36 +0530

    using the transition events

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,15 @@
   </div>
 
   <div class='container'>
-    <transition name='para'>
+    <transition
+      name='para'
+      @before-enter='beforeEnter'
+      @enter='enter'
+      @after-enter='afterEnter'
+      @before-leave='beforeLeave'
+      @leave='leave'
+      @after-leave='afterLeave'
+    >
       <p v-if='paraIsVisible'>This is only sometimes visible...</p>
     </transition>
     <button @click='toggleParagraph'>Toggle Paragraph</button>
@@ -39,6 +47,31 @@ export default {
     };
   },
   methods: {
+    beforeEnter(el){
+      console.log('beforeEnter');
+      console.log(el);
+    },
+
+    enter(el){
+      console.log('enter');
+      console.log(el);
+    },
+    afterEnter(el){
+      console.log('afterEnter');
+      console.log(el);
+    },
+    beforeLeave(el){
+      console.log('beforeLeave');
+      console.log(el);
+    },
+    leave(el){
+      console.log('leave');
+      console.log(el);
+    },
+    afterLeave(el){
+      console.log('afterLeave');
+      console.log(el);
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
