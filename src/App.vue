2d48ef50891565b5498c9d33ba454be8dfbc24a6
Raman Goyal <ramangoyal@Ramans-MacBook-Air.local>
2021-07-22 22:26:15 +0530

    Animate using transition tags of Vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <div class="block" :class='{animate:animatedBlock}'></div>
     <button @click='animateBlock'>Animate</button>
   </div>
+  <div class='container'>
+    <transition>
+      <p v-if='paraIsVisible'>This is only sometimes visible...</p>
+    </transition>
+    <button @click='toggleParagraph'>Toggle Paragraph</button>
+  </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -18,6 +24,7 @@ export default {
     return {
       animatedBlock:false,
       dialogIsVisible: false,
+      paraIsVisible:false,
     };
   },
   methods: {
@@ -29,6 +36,9 @@ export default {
     },
     animateBlock(){
       this.animatedBlock=!this.animatedBlock;
+    },
+    toggleParagraph(){
+      this.paraIsVisible=!this.paraIsVisible
     }
   },
 };
@@ -79,7 +89,34 @@ button:active {
 .animate{
   /*transform: translateX(-150px);*/
   animation: slide-fade 0.3s ease-out forwards;
+}
 
+.v-enter-from{
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active{
+  transition: all 0.3s ease-out;
+}
+
+.v-leave-from{
+  opacity: 0;
+  transform: translateY(0);
+}
+
+.v-leave-active{
+  transition: all 0.3s ease-in;
+}
+
+.v-leave-to{
+  opacity: 0;
+  transform: translateY(-30);
+}
+
+.v-enter-to{
+  opacity: 1;
+  transform: translateY(0);
 }
 
 @keyframes slide-fade {
